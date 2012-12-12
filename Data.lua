ac8173093921f8744245b6ebce449f8a5e3ec18e
Ethan Centaurai <stevenblanchard86@gmail.com>
2012-12-12 19:17:26 +0000

    Commit empty data file to be populated later.

---
+++ Data.lua
@@ -0,0 +1,20 @@
+
+local _, LunarFestival = ...
+local points = LunarFestival.points -- points[<mapfile>] = { [<coordinates>] = <quest ID> }
+
+-- Eastern Kingdoms
+
+
+-- Kalimdor
+
+
+-- Outland
+
+
+-- Northrend
+
+
+-- Cataclysm
+
+
+-- Pandaria
