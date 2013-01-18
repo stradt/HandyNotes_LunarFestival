ea13eaa932f2828a02b3ca50aad4f5479db164da
Ethan Centaurai <stevenblanchard86@gmail.com>
2013-01-18 11:29:13 +0000

    Added data for Eastern Kingdoms.

--- Data.lua
+++ Data.lua
@@ -1,9 +1,68 @@
 
 local _, LunarFestival = ...
-local points = LunarFestival.points -- points[<mapfile>] = { [<coordinates>] = <quest ID> }
+local points = LunarFestival.points
+-- points[<mapfile>] = { [<coordinates>] = { <quest ID>, <achievement ID>, <achievement criteria ID> } }
+
 
 -- Eastern Kingdoms
+points["BlastedLands"] = {
+	[54304950] = { 8647, 912, 2 }, -- Elder Bellowrage
+}
 
+points["BurningSteppes"] = {
+	[54202420] = { 8683, 912, 10 }, -- Elder Dawnstrider
+	[69904480] = { 8636, 912, 9 }, -- Elder Rumblerock
+}
+
+points["DunMorogh"] = {
+	[53904980] = { 8653, 912, 1 }, -- Elder Goldwell
+}
+
+points["EasternPlaguelands"] = {
+	[35206810] = { 8688, 912, 15 }, -- Elder Windrun
+	[75705450] = { 8650, 912, 16 }, -- Elder Snowcrown
+}
+
+points["Elwynn"] = {
+	[40006300] = { 8649, 912, 3 }, -- Elder Stormbrow
+}
+
+points["Hinterlands"] = {
+	[50004800] = { 8643, 912, 11 }, -- Elder Highpeak
+}
+
+points["LochModan"] = {
+	[33414651] = { 8642, 912, 7 }, -- Elder Silvervein
+}
+
+points["SearingGorge"] = {
+	[21007800] = { 8651, 912, 12 }, -- Elder Ironband
+}
+
+points["Silverpine"] = {
+	[45004110] = { 8645, 912, 14 }, -- Elder Obsidian
+}
+
+points["StranglethornJungle"] = {
+	[71003400] = { 8716, 912, 5 }, -- Elder Starglade
+}
+
+points["TheCapeOfStranglethorn"] = {
+	[40007250] = { 8674, 912, 6 }, -- Elder Winterhoof
+}
+
+points["Tirisfal"] = {
+	[61005300] = { 8652, 912, 13 }, -- Elder Graveborn
+}
+
+points["WesternPlaguelands"] = {
+	[69207340] = { 8714, 912, 17 }, -- Elder Moonstrike
+	[63503610] = { 8722, 912, 4 }, -- Elder Meadowrun
+}
+
+points["Westfall"] = {
+	[56604710] = { 8675, 912, 8 }, -- Elder Skychaser
+}
 
 -- Kalimdor
 
