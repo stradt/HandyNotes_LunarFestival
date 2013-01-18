741df390675d8b7c75a003274179545c775f4fb3
Ethan Centaurai <stevenblanchard86@gmail.com>
2013-01-18 14:42:46 +0000

    Add data for Kalimdor.

--- Data.lua
+++ Data.lua
@@ -65,7 +65,71 @@ points["Westfall"] = {
 }
 
 -- Kalimdor
+points["Ashenvale"] = {
+	[35504890] = { 8725, 911, 9 }, -- Elder Riversong
+}
 
+points["Aszhara"] = {
+	[64707930] = { 8720, 911, 2 }, -- Elder Skygleam
+}
+
+points["Barrens"] = {
+	[48505920] = { 8717, 911, 3 }, -- Elder Moonwarden
+	[68305990] = { 8680, 911, 5 }, -- Elder Windtotem
+}
+
+points["Darkshore"] = {
+	[49501890] = { 8721, 911, 7 }, -- Elder Starweave
+}
+
+points["Durotar"] = {
+	[53204360] = { 8670, 911, 1 }, -- Elder Runetotem
+}
+
+points["Feralas"] = {
+	[62563100] = { 8685, 911, 11 }, -- Elder Mistwalker
+	[76703790] = { 8679, 911, 10 }, -- Elder Grimtotem
+}
+
+points["Felwood"] = {
+	[38305280] = { 8723, 911, 12 }, -- Elder Nightwind
+}
+
+points["Mulgore"] = {
+	[58405320] = { 8673, 911, 8 }, -- Elder Bloodhoof
+}
+
+points["Silithus"] = {
+	[30800330] = { 8654, 911, 20 }, -- Elder Primestone
+	[53003540] = { 8719, 911, 21 }, -- Elder Bladesing
+}
+
+points["SouthernBarrens"] = {
+	[41504740] = { 8686, 911, 4 }, -- Elder Highmountain
+}
+
+points["Tanaris"] = {
+	[37207900] = { 8671, 911, 15 }, -- Elder Ragetotem
+	[51402880] = { 8684, 911, 16 }, -- Elder Dreamseer
+}
+
+points["Teldrassil"] = {
+	[56805310] = { 8715, 911, 6 }, -- Elder Bladeleaf
+}
+
+points["ThousandNeedles"] = {
+	[46305100] = { 8682, 911, 13 }, -- Elder Skyseer
+	[77007560] = { 9724, 911, 14 }, -- Elder Morningdew
+}
+
+points["UngoroCrater"] = {
+	[50307610] = { 8681, 911, 17 }, -- Elder Thunderhorn
+}
+
+points["Winterspring"] = {
+	[53205670] = { 8726, 911, 18 }, -- Elder Brightspear
+	[59904990] = { 8672, 911, 19 }, -- Elder Stonespire
+}
 
 -- Outland
 
